78dd50c0caead3c13036716ff71be91ff73c0227
Unknown <nesterovsusu@gmail.com>
2019-03-06 21:28:59 +0500

    copywprt-fix
    .

--- copywprt/copywprt/copywprt.cpp
+++ copywprt/copywprt/copywprt.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int* copy(int* ptr, int n) {
 	int* arr2 = new int[n];
 	for (int i = 0; i < n; i++) {
-		arr2[i] = *(ptr + i);
+		arr2[i] = ptr[i];
 	}
-	int* ptr2 = arr2;
-	return ptr2;
+	return arr2;
 }
 
 int main()
@@ -20,9 +19,8 @@ int main()
 	for (int i = 0; i < n; i++) {
 		cin >> arr1[i];
 	}
-	int* ptr1 = arr1;
-	int* ptr2 = copy(ptr1, n);
+	int* ptr2 = copy(arr1, n);
 	for (int i = 0; i < n; i++) {
-		cout << *(ptr2 + i);
+		cout << ptr2[i];
 	}
 }
